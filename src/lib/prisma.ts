7dc39f662c5fbdf41403d3b69dc2b18016aa2e74
andmohiko <andmohiko@gmail.com>
2026-03-28 18:30:46 +0900

    fix: prisma to v6

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,9 @@
-import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@/generated/prisma/client"
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter: new PrismaPg(process.env.DATABASE_URL!),
-  })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
