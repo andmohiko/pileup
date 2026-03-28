7014e468a7e77ad89d10d5fa3b4979bdf94437ee
andmohiko <andmohiko@gmail.com>
2026-03-28 18:56:34 +0900

    fix: save user on login

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server"
 import { createClient } from "@/lib/supabase/server"
+import { prisma } from "@/lib/prisma"
 
 export const GET = async (request: Request) => {
   const { searchParams, origin } = new URL(request.url)
@@ -10,6 +11,21 @@ export const GET = async (request: Request) => {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser()
+
+      if (user) {
+        await prisma.user.upsert({
+          where: { id: user.id },
+          update: {},
+          create: {
+            id: user.id,
+            email: user.email!,
+          },
+        })
+      }
+
       return NextResponse.redirect(`${origin}${next}`)
     }
   }
